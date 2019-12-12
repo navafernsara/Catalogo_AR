4546a64ffb3499f0ca72f85229970bfc3e0201ce
navafernsara <SaraNF@hotmail.es>
2019-12-12 12:03:59 +0100

    SNF - Catalogo_AR - 12/12/19
    SNF - Catalogo_AR - 12/12/19

---
+++ Assets/SNF-Script/RotateScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateScript : MonoBehaviour
+{
+    public Slider slider;
+
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, slider.value, 0); 
+    }
+}
